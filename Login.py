a7411cbea8499525ac8b55c5589d870032401b7e
yaya <fadiyayaa@gmail.com>
2024-05-17 16:10:16 +0700

    fix :hapus code line

--- Login.py
+++ Login.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Toplevel
 def regist():
-    login.destroy
     OUTPUT_PATH = Path(__file__).parent
     ASSETS_PATH = OUTPUT_PATH / Path(r"D:\Praktikum Prokom\TUBES PRAKPROK!!!\registrasi\assets\frame0")
 
